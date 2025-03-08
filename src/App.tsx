d3247a3b3444cc5193751a0893af383370de74ab
chengetanai <mukanhairic@gmail.com>
2025-03-08 15:24:44 +0200

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/navbar";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <>
-      <div className="bg-[#030303] text-white">
+      <div className="bg-[#030303] text-white h-screen">
         <NavBar />
+        <Hero />
       </div>
     </>
   );
---
+++ src/components/hero.tsx
@@ -0,0 +1,34 @@
+import { Button } from "./ui/button";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center mt-32">
+      <div className="flex flex-col gap-6 text-center">
+        <h1 className="text-7xl">Traffic Flow Modelling</h1>
+        <h1 className="text-6xl text-green-300">Machine Learning</h1>
+        <div className="flex flex-col gap-2">
+          <p>
+            Unlock the power of machine learning to forecast and improve traffic
+            flow efficiency.
+          </p>
+          <p>
+            Start your project with a Postgres database, Authentication, instant
+            APIs, Edge
+          </p>
+          <p>
+            Functions, Realtime subscriptions, Storage, and Vector embeddings.
+          </p>
+        </div>
+        <div className="flex items-center justify-around mt-6">
+          <Button className="bg-green-400 text-black hover:bg-white hover:cursor-pointer">
+            Start Project
+          </Button>
+          <Button className="bg-green-400 text-black hover:bg-white hover:cursor-pointer">
+            Request a Demo
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
